41bfeb484ca20223fe88aec2a446a701521ed71e
paulniu <niupuyue@renruihr.com>
2019-05-17 18:39:54 +0800

    Initial commit

---
+++ app/src/main/java/com/paulniu/mykeyboarddemo/MyChatKeyboardView.java
@@ -0,0 +1,33 @@
+package com.paulniu.mykeyboarddemo;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+/**
+ * Coder: niupuyue
+ * Date: 2019/5/17
+ * Time: 18:25
+ * Desc:
+ * Version:
+ */
+public class MyChatKeyboardView extends LinearLayout {
+
+    private Context mContext;
+
+    public MyChatKeyboardView(Context context) {
+        super(context);
+    }
+
+    public MyChatKeyboardView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public MyChatKeyboardView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+
+
+}
